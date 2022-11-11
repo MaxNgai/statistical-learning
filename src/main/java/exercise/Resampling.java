c4dda764a254d57ac0fad1075ab5d0e04907bf12
Max <545296992@qq.com>
2022-11-12 02:36:48 +0800

    handcraft validation set

---
+++ src/main/java/exercise/Resampling.java
@@ -0,0 +1,62 @@
+package exercise;
+
+import data.Auto;
+import org.apache.commons.math3.linear.*;
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import org.apache.commons.math3.util.Pair;
+import org.junit.Test;
+import tool.CrossValidation;
+import tool.DefaultVectorChangingVisitor;
+import tool.Macro;
+
+/**
+ * @author Max Ngai
+ * @since 2022/11/12
+ */
+public class Resampling {
+    Auto auto = new Auto();
+
+
+    /**
+     * p191
+     */
+    @Test
+    public void validationSet() {
+
+        Pair<RealMatrix, RealMatrix> raw = CrossValidation.validationSet(auto.getAuto(), 196);
+        RealMatrix train = raw.getFirst();
+        RealMatrix test = raw.getSecond();
+        RealVector testX = test.getColumnVector(3);
+        RealVector testY = test.getColumnVector(0);
+        RealVector trainX = train.getColumnVector(3);
+        RealVector trainY = train.getColumnVector(0);
+
+        Array2DRowRealMatrix linearTrainXMatrix = Macro.matrixHConcat(trainX.toArray());
+        Array2DRowRealMatrix linearTestMatrix = Macro.matrixHConcat(testX.toArray());
+        seeMse(trainY, linearTrainXMatrix, testY, linearTestMatrix);
+
+        Array2DRowRealMatrix quadraticTrainXMatrix = Macro.matrixHConcat(trainX.ebeMultiply(trainX).toArray(), trainX.toArray());
+        Array2DRowRealMatrix quadraticTestXMatrix = Macro.matrixHConcat(testX.ebeMultiply(testX).toArray(), testX.toArray());
+        seeMse(trainY, quadraticTrainXMatrix, testY, quadraticTestXMatrix);
+
+        Array2DRowRealMatrix cubicTrainXMatrix = Macro.matrixHConcat(trainX.ebeMultiply(trainX).ebeMultiply(trainX).toArray(), trainX.ebeMultiply(trainX).toArray(), trainX.toArray());
+        Array2DRowRealMatrix cubicTestXMatrix = Macro.matrixHConcat(testX.ebeMultiply(testX).ebeMultiply(testX).toArray(), testX.ebeMultiply(testX).toArray(), testX.toArray());
+        seeMse(trainY, cubicTrainXMatrix, testY, cubicTestXMatrix);
+    }
+
+    private static double seeMse(RealVector trainY, RealMatrix trainX, RealVector testY, RealMatrix testX) {
+        OLSMultipleLinearRegression rg = new OLSMultipleLinearRegression();
+        rg.newSampleData(trainY.toArray(), trainX.getData());
+        ArrayRealVector param = new ArrayRealVector(rg.estimateRegressionParameters());
+        double b = param.getEntry(0);
+        RealVector k = param.getSubVector(1, param.getDimension() - 1);
+
+        RealVector dev = testX.transpose().preMultiply(k).add(new ArrayRealVector(testX.getRowDimension(), b)).subtract(testY);
+        double mse = dev.dotProduct(dev) / dev.getDimension();
+        System.out.println(mse);
+        return mse;
+
+    }
+
+}
---
+++ src/main/java/tool/CrossValidation.java
@@ -0,0 +1,39 @@
+package tool;
+
+import com.google.common.primitives.Ints;
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.util.Pair;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * @author Max Ngai
+ * @since 2022/11/12
+ */
+public class CrossValidation {
+
+    public static Pair<RealMatrix, RealMatrix> validationSet(RealMatrix m, int sizeOfTrain) {
+        List<double[]> train = new ArrayList<>();
+        List<double[]> test = new ArrayList<>();
+
+        List<Integer> collect = IntStream.range(0, m.getRowDimension()).boxed().collect(Collectors.toList());
+        Collections.shuffle(collect);
+
+        for (int i = 0; i < collect.size(); i++) {
+            if (i < m.getRowDimension() / 2) {
+                train.add(m.getRow(collect.get(i)));
+            } else {
+                test.add(m.getRow(collect.get(i)));
+            }
+        }
+
+        return Pair.create(
+                Macro.matrixVConcat(train),
+                Macro.matrixVConcat(test)
+        );
+
+    }
+}
