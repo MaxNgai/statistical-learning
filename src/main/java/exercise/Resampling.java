7eace99c046c7c4550fcc4b9146de56feb4fad5d
Max <545296992@qq.com>
2022-11-12 12:23:22 +0800

    loocv

--- src/main/java/exercise/Resampling.java
+++ src/main/java/exercise/Resampling.java
@@ -33,18 +33,18 @@ public class Resampling {
 
         Array2DRowRealMatrix linearTrainXMatrix = RegressionUtil.polynomial(trainX.toArray(), 1);
         Array2DRowRealMatrix linearTestMatrix = RegressionUtil.polynomial(testX.toArray(), 1);
-        seeMse(trainY, linearTrainXMatrix, testY, linearTestMatrix);
+        seeRegressionMse(trainY, linearTrainXMatrix, testY, linearTestMatrix);
 
         Array2DRowRealMatrix quadraticTrainXMatrix = RegressionUtil.polynomial(trainX.toArray(), 2);
         Array2DRowRealMatrix quadraticTestXMatrix = RegressionUtil.polynomial(testX.toArray(), 2);
-        seeMse(trainY, quadraticTrainXMatrix, testY, quadraticTestXMatrix);
+        seeRegressionMse(trainY, quadraticTrainXMatrix, testY, quadraticTestXMatrix);
 
         Array2DRowRealMatrix cubicTrainXMatrix = RegressionUtil.polynomial(trainX.toArray(), 3);
         Array2DRowRealMatrix cubicTestXMatrix = RegressionUtil.polynomial(testX.toArray(), 3);
-        seeMse(trainY, cubicTrainXMatrix, testY, cubicTestXMatrix);
+        seeRegressionMse(trainY, cubicTrainXMatrix, testY, cubicTestXMatrix);
     }
 
-    private static double seeMse(RealVector trainY, RealMatrix trainX, RealVector testY, RealMatrix testX) {
+    private static double seeRegressionMse(RealVector trainY, RealMatrix trainX, RealVector testY, RealMatrix testX) {
         OLSMultipleLinearRegression rg = new OLSMultipleLinearRegression();
         rg.newSampleData(trainY.toArray(), trainX.getData());
         ArrayRealVector param = new ArrayRealVector(rg.estimateRegressionParameters());
@@ -53,9 +53,44 @@ public class Resampling {
 
         RealVector dev = testX.transpose().preMultiply(k).add(new ArrayRealVector(testX.getRowDimension(), b)).subtract(testY);
         double mse = dev.dotProduct(dev) / dev.getDimension();
-        System.out.println(mse);
         return mse;
 
     }
+
+    CrossValidation.CvMseGetter regressionMseGetter = new CrossValidation.CvMseGetter() {
+        @Override
+        public <TEX extends RealMatrix, TEY extends RealVector, TRX extends RealMatrix, TRY extends RealVector> double testSetMse(TRX trainX, TRY trainY, TEX testX, TEY testY) {
+            return seeRegressionMse(trainY, trainX, testY, testX);
+        }
+    };
+
+    /**
+     * p192 loocv
+     */
+    @Test
+    public void loocv() {
+        ArrayRealVector y = new ArrayRealVector(auto.getMpg());
+        Array2DRowRealMatrix x = Macro.hstack(auto.getHorsePower());
+
+        double avgMse = CrossValidation.loocvMse(x, y, regressionMseGetter);
+        System.out.println(avgMse);
+    }
+
+    /**
+     * p193
+     */
+    @Test
+    public void polynomialLoocv() {
+        int power = 5;
+        for (int i = 1; i <= power; i++) {
+            ArrayRealVector y = new ArrayRealVector(auto.getMpg());
+            Array2DRowRealMatrix x = RegressionUtil.polynomial(auto.getHorsePower(), i);
+            double avgMse = CrossValidation.loocvMse(x, y, regressionMseGetter);
+            System.out.println(avgMse);
+        }
+    }
+
+
+
 
 }
--- src/main/java/tool/CrossValidation.java
+++ src/main/java/tool/CrossValidation.java
@@ -1,9 +1,14 @@
 package tool;
 
+import org.apache.commons.math3.linear.ArrayRealVector;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
 import org.apache.commons.math3.util.Pair;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -33,5 +38,47 @@ public class CrossValidation {
                 Macro.vstack(test)
         );
 
+    }
+
+    /**
+     * use loocv to yield the mse
+     * @return
+     */
+    public static double loocvMse(RealMatrix x, RealVector y, CvMseGetter getMse) {
+        int n = y.getDimension();
+        int p = x.getColumnDimension();
+        List<Integer> rows = IntStream.range(0, n).boxed().collect(Collectors.toList());
+        int[] selectedColumns = IntStream.range(0, p).toArray();
+
+        return IntStream.range(0, n)
+                .boxed()
+                .map(i -> {
+                    ArrayList<Integer> removed = new ArrayList<>(rows);
+                    removed.remove(i);
+                    int[] selectedRows = removed.stream().mapToInt(e -> e).toArray();
+                    RealMatrix trainX = x.getSubMatrix(selectedRows, selectedColumns);
+                    RealMatrix testX = x.getSubMatrix(new int[]{i}, selectedColumns);
+                    ArrayRealVector testY = new ArrayRealVector(new double[]{y.getEntry(i)});
+                    List<Double> rawTrainY = Arrays.stream(y.toArray()).boxed().collect(Collectors.toList());
+                    rawTrainY.remove(i.intValue());
+                    ArrayRealVector trainY = new ArrayRealVector(Macro.toArray(rawTrainY));
+
+                    double mse = getMse.testSetMse(trainX, trainY, testX, testY);
+                    return mse;
+                }).mapToDouble(e -> e).average().getAsDouble();
+
+    }
+
+    /**
+     * functional-interface that use trainX & train Y to train model,
+     * the see the mse on testSet(testX, testY)
+     */
+    public interface CvMseGetter {
+
+        <TEX extends RealMatrix,
+        TEY extends RealVector,
+        TRX extends RealMatrix,
+        TRY extends RealVector>
+        double testSetMse(TRX trainX, TRY trainY, TEX testX, TEY testY);
     }
 }
