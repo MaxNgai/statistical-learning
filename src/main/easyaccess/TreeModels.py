a36c3cbc7e678edb9d50b9019396581b9c6b5e26
Max <545296992@qq.com>
2023-03-24 02:18:15 +0800

    easy-access: gbdt & stacking

--- src/main/easyaccess/TreeModels.py
+++ src/main/easyaccess/TreeModels.py
@@ -1,7 +1,8 @@
 import numpy as np
+from sklearn.ensemble import StackingClassifier
 from sklearn import tree
 from sklearn.ensemble import BaggingClassifier
-
+from sklearn.ensemble import GradientBoostingClassifier
 
 #from sklearn.tree import DecisionTreeClassifier
 #from sklearn.tree import DecisionTreeRegressor
@@ -40,3 +41,23 @@ def randomForest(trainX, trainY, testX, testY):
     prob = model.predict_proba(testX)
 
     trees = model.estimators_
+
+#from sklearn.ensemble import GradientBoostingClassifier
+#from sklearn.ensemble import GradientBoostingRegressor
+def gbdt(trainX, trainY, testX, testY):
+    model = GradientBoostingClassifier(learning_rate=0.1, n_estimators=100, subsample=1, max_depth=3)
+    yhat = model.predict(testX)
+    indexOfLeaves = model.apply(testX)
+    score = model.score(testX, testY)    
+    prob = model.predict_proba(testX)
+    print(model.feature_importances_)
+    trees = model.estimators_
+
+#from sklearn.ensemble import StackingClassifier
+#from sklearn.ensemble import StackingRegressor
+def stackedGeneralization(trainX, trainY, testX, testY):
+    estimators = [ GradientBoostingClassifier(), BaggingClassifier()]
+    model = StackingClassifier(estimators=estimators, final_estimator=LogisticRegression())
+    yhat = model.predict(testX)
+    score = model.score(testX, testY)    
+    prob = model.predict_proba(testX)
